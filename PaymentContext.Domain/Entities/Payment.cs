a39c3a424d991a963e5efd7a0b217617011c1035
Vinicius Almeida <termobin@gmail.com>
2023-05-07 20:27:33 -0300

    Created Project and Entities

---
+++ PaymentContext.Domain/Entities/Payment.cs
@@ -0,0 +1,15 @@
+namespace PaymentContext.Domain.Entities;
+
+public abstract class Payment
+{
+    public string Number { get; set; }
+    public DateTime PaidDate { get; set; }
+    public DateTime ExpireDate { get; set; }
+    public decimal Total { get; set; }
+    public decimal TotalPaid { get; set; }
+    public string Document { get; set; }
+    public string Payer { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+
+}
